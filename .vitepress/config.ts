d74efd18404b26ca814172c3b80c94991b9614d9
wangjie36 <49926816+wangyewei@users.noreply.github.com>
2024-05-24 15:27:53 +0800

    feat: added HeaderLogoArea

---
+++ .vitepress/config.ts
@@ -0,0 +1,13 @@
+import { defineConfigWithTheme } from 'vitepress'
+import type { VPYevTheme } from 'vitepress-theme-yev'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfigWithTheme<VPYevTheme>({
+  title: "Yev",
+  description: "Yev's personal blog site",
+  themeConfig: {
+    nav: {
+      logo: 'avator.jpg'
+    }
+  }
+})
--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -2,9 +2,20 @@ import { Theme } from 'vitepress'
 import Layout from './app/Layout'
 import DefaultTheme from 'vitepress/theme'
 
-import './app/styles/index.css'
-
 export default {
   ...DefaultTheme,
-  Layout: Layout
+  Layout,
+  enhanceApp({ app }) {
+    /**
+     * `{() => component}` is too ugly to me, so I'd rather give
+     * up better perfomance 
+     */
+    app.config.warnHandler = msg => {
+      if (msg.includes('Non-function value encountered for default slot')) {
+        return
+      }
+    }
+  }
 } satisfies Theme
+
+export * from './composables/defineTheme'
