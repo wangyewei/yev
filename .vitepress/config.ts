1ee7385630fd859adbc174ff938c6dc474c5ebed
王野未 <826036140@qq.com>
2024-05-27 00:58:56 +0800

    chore: sync update config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,7 +7,22 @@ export default defineConfigWithTheme<VPYevTheme>({
   description: "Yev's personal blog site",
   themeConfig: {
     nav: {
-      logo: 'https://avatars.githubusercontent.com/u/49926816?v=4'
+      logo: 'https://avatars.githubusercontent.com/u/49926816?v=4',
+      items: [
+        { title: '首页', path: '/', icon: 'FaSolidDotCircle' },
+        {
+          title: '技术', path: '/techology', children: [
+            { title: 'typescript', path: '/typescript' },
+            { title: 'git', path: '/git' }
+          ]
+        },
+        {
+          title: '手稿', path: '/type', children: [
+            { title: '三生（上）', path: '/love-3' }
+          ]
+        },
+        { title: '心绪', path: '/emotions' },
+      ]
     }
   }
 })
