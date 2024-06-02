97c410fdabcabbeded5f2b3c6de547c93c0cf612
王野未 <826036140@qq.com>
2024-06-03 01:16:52 +0800

    https://github.com/wangyewei/vitepress-theme-yev/commit/4f8a7404af996df34490dc2443724c6c14927f92

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,25 +3,48 @@ import type { VPYevTheme } from 'vitepress-theme-yev'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfigWithTheme<VPYevTheme>({
-  title: "Yev",
+  title: 'Yev',
   description: "Yev's personal blog site",
   themeConfig: {
     nav: {
       logo: '/avatar.jpg',
       items: [
-        { title: '首页', path: '/', icon: 'FaSolidDotCircle' },
         {
-          title: '技术', path: '/techology', icon: 'FaSolidCode', children: [
-            { title: 'typescript', path: '/typescript' },
-            { title: 'git', path: '/git' }
+          title: '首页',
+          path: '/',
+          icon: 'FaSolidDotCircle',
+          children: [
+            { title: '自述', path: 'intro' },
+            { title: '赞助', path: 'sponsor' }
           ]
         },
         {
-          title: '手稿', path: '/type', icon: 'FaFilledQulliPen', children: [
-            { title: '三生（上）', path: '/love-3' }
+          title: '技术',
+          path: '/techology',
+          icon: 'FaSolidCode',
+          children: [
+            { title: 'TypeScript', path: '/typescript' },
+            { title: 'Git', path: '/git' },
+            { title: 'GIS', path: '/gis' },
+            { title: '数字孪生', path: '/digi' },
+            { title: 'Vue', path: '/vue' }
           ]
         },
-        { title: '心绪', icon: 'FaSolidEmotion', path: '/emotions/' },
+        {
+          title: '手稿',
+          path: '/type',
+          icon: 'FaFilledQulliPen',
+          children: [{ title: '三生（上）', path: '/love-3' }]
+        },
+        {
+          title: '心绪',
+          icon: 'FaSolidEmotion',
+          path: '/emotions/',
+          children: [
+            { title: '日记', path: '/diary' },
+            { title: '书摘', path: '/excerpt' }
+          ]
+        }
       ]
     }
   }
