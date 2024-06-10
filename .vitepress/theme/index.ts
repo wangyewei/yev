b7530c398b8a1078f8967769c297339779bd3d55
王野未 <826036140@qq.com>
2024-06-11 02:34:22 +0800

    chore:code merge

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import type { Theme } from 'vitepress'
 import YevTheme from 'vitepress-theme-yev'
+import 'vitepress-theme-yev/dist/styles/index.css'
 export default {
   ...YevTheme
 } satisfies Theme
