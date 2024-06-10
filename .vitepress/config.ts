5bc095a520dee77672ac02d4bdec64605b11775c
王野未 <826036140@qq.com>
2024-06-10 22:57:08 +0800

    chore: https://github.com/wangyewei/vitepress-theme-yev/commit/64f56296d02b913db6665cb0a10fd27aaab5d2f6

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,6 +6,10 @@ export default defineConfigWithTheme<VPYevTheme>({
   title: 'Yev',
   description: "Yev's personal blog site",
   themeConfig: {
+    copyright: {
+      name: 'Yev Wang',
+      startYear: '2024'
+    },
     nav: {
       logo: '/avatar.jpg',
       items: [
@@ -14,7 +18,7 @@ export default defineConfigWithTheme<VPYevTheme>({
           path: '/',
           icon: 'FaSolidDotCircle',
           children: [
-            { title: '自述', path: 'intro' },
+            { title: '自述', path: 'en/about/me' },
             { title: '赞助', path: 'sponsor' }
           ]
         },
@@ -44,6 +48,66 @@ export default defineConfigWithTheme<VPYevTheme>({
             { title: '日记', path: '/diary' },
             { title: '书摘', path: '/excerpt' }
           ]
+        }
+      ]
+    },
+    footer: {
+      links: [
+        {
+          name: '关于',
+          links: [
+            { name: '关于本站', href: '/abouts/site' },
+            { name: '关于我', href: '/abouts/me' },
+            {
+              name: '关于此项目',
+              href: 'https://github.com/wangyewei/vitepress-theme-yev'
+            }
+          ]
+        },
+        {
+          name: '联系',
+          links: [
+            { name: '邮件', href: 'mailto:yevwang@qq.com' },
+            {
+              name: 'GitHub',
+              href: 'https://github.com/wangyewei'
+            }
+          ]
+        },
+        {
+          name: '更多',
+          links: [
+            { name: '软件开发', href: 'https://06k4.com' },
+            { name: '行业解决方案', href: 'https://06k4.com' }
+          ]
+        }
+      ],
+      template: [
+        {
+          type: 'span',
+          children: [
+            {
+              type: 'div',
+              text: 'Powerd by Vercel'
+            },
+            {
+              type: 'span',
+              text: '|'
+            },
+            {
+              type: 'span',
+              text: 'Designed by Shiro'
+            },
+            {
+              type: 'span',
+              text: '|'
+            },
+            {
+              type: 'span',
+              text: 'Based on Vitepress-theme-yev'
+            }
+          ],
+          className: 'flex items-center gap-[4px] flex-wrap'
         }
       ]
     }
---
+++ en/about/me.md
@@ -0,0 +1,15 @@
+---
+title: Introduce
+---
+
+# Hello.
+
+**My name is Yev Wang.**
+
+I am a software engineer at [Asiainfo Inc.](https://asiainfo.com), currently based in Chengdu, China. My primary skill set encompasses Vue, React, Svelte, Flutter, and TypeScript.
+
+In addition to my [GitHub](https://github.com/wangyewei), you can find me on [Twitter](https://twitter.com/yev_wang), where you can get in touch with me. If you happen to speak Chinese, my Chinese name is 王野未 (wǎng yě wèi). You can also communicate with me in Chinese.
+
+Outside of programming, I enjoy coffee (dark roast-only), music, and watching films. Recently, I've been writing some [letters](#), even though they were never sent, and occasionally composing [poems](#).
+
+[Anything to add?](#)
